ad1b7122d792e07e37d92ad03839d4af168f5f1f
Koby <kobysac@gmail.com>
2023-10-02 23:20:15 -0700

    created more questions and default cases

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# Title
+# Default Title
 
 ## Description
-
+Default description
 ## Table of Contents
-
+Default contents
 ## Installation
-
+Default installation
 ## Usage
-
+Default usage
 ## License
-
+Default license
 ## Contributing
-
+Default contribute
 ## Tests
-
+Default tests
 ## Questions
+Default questions
--- index.js
+++ index.js
@@ -7,9 +7,49 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'input',
-        message: 'Type something!',
-        name: 'username',
-    }
+        message: 'What is the title of your project/application?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a quick description of what the app is for.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'List Table of Contents:',
+        name: 'contents',
+    },
+    {
+        type: 'input',
+        message: 'How do you install this app?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How do you use this app?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Choose a license for your app:',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'How would a fan contribute to this project?',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'What tests have been done?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Any questions?',
+        name: 'questions',
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -22,24 +62,48 @@ function writeToFile(fileName, data) {
 
 function formatData(data) {
 
+    console.log(data, "---");
+    let {
+        title,
+        description,
+        contents,
+        installation,
+        usage,
+        license,
+        contribute,
+        tests,
+        questions
+    } = data;
+
+    title = title.length == 0 ? "Default Title" : title;
+    description = description.length == 0 ? "Default description" : description;
+    contents = contents.length == 0 ? "Default contents" : contents;
+    installation = installation.length == 0 ? "Default installation" : installation;
+    usage = usage.length == 0 ? "Default usage" : usage;
+    license = license.length == 0 ? "Default license" : license;
+    contribute = contribute.length == 0 ? "Default contribute" : contribute;
+    tests = tests.length == 0 ? "Default tests" : tests;
+    questions = questions.length == 0 ? "Default questions" : questions;
+
     const formattedString =
-`# Title
+        `# ${title}
 
 ## Description
-
+${description}
 ## Table of Contents
-
+${contents}
 ## Installation
-
+${installation}
 ## Usage
-
+${usage}
 ## License
-
+${license}
 ## Contributing
-
+${contribute}
 ## Tests
-
+${tests}
 ## Questions
+${questions}
 `;
 
     // return JSON.stringify(data);
@@ -55,6 +119,9 @@ function init() {
 
             console.log(formattedData, "---------");
             writeToFile("README.md", formattedData);
+        })
+        .catch((err) => {
+            console.log(err);
         });
 }
 
